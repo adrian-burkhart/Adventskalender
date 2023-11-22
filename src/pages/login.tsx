c8d9f6fd13fd6efd3201c071550e0bb92a9bcf30
adrian-burkhart <adrian.burkhart@gmail.com>
2023-11-22 17:16:15 +0100

    customize login theme

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -19,10 +19,62 @@ const LoginPage = () => {
     return (
       <Auth
         redirectTo="/"
-        appearance={{ theme: ThemeSupa }}
         supabaseClient={supabaseClient}
-        providers={["google", "github"]}
-        socialLayout="horizontal"
+        providers={[]}
+        localization={{
+          variables: {
+            sign_in: {
+              email_label: "Deine E-Mail-Adresse",
+              password_label: "Dein Passwort",
+              email_input_placeholder: "Deine E-Mail-Adresse",
+              password_input_placeholder: "Dein Passwort",
+              button_label: "Einloggen",
+              loading_button_label: "Wird eingeloggt...",
+              link_text:
+                "Du hast schon einen Account? Dann kannst du dich hier einloggen.",
+            },
+            sign_up: {
+              email_label: "Deine E-Mail-Adresse",
+              password_label: "Dein Passwort",
+              email_input_placeholder: "Deine E-Mail-Adresse",
+              password_input_placeholder: "Dein Passwort",
+              button_label: "Registrieren",
+              loading_button_label: "Wird registriert...",
+              link_text:
+                "Du hast keinen Account? Dann kannst du dich hier registrieren.",
+              confirmation_text:
+                "Du bekommst eine E-Mail mit einem Bestätigungslink.",
+            },
+            forgotten_password: {
+              email_label: "Deine E-Mail-Adresse",
+              password_label: "Dein Passwort",
+              email_input_placeholder: "Deine E-Mail-Adresse",
+              button_label: "Passwort zurücksetzen (E-Mail senden)",
+              loading_button_label: "E-Mail wird gesendet...",
+              link_text: "Du hast dein Passwort vergessen?",
+              confirmation_text:
+                "Schaue in deine E-Mails für den Link zum Zurücksetzen deines Passwortes.",
+            },
+            update_password: {
+              password_label: "Dein neues Passwort",
+              password_input_placeholder: "Dein neues Passwort",
+              button_label: "Passwort aktualisieren",
+              loading_button_label: "Passwort wird aktualisiert...",
+              confirmation_text: "Dein Passwort wurde aktualisiert.",
+            },
+          },
+        }}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#ef4444",
+                brandAccent: "darkred",
+              },
+            },
+          },
+        }}
       />
     )
   }
